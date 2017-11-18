0f47f974ec1d8d56495f4310e187af7d038ed259
Byteshaper <henning@byteshaper.com>
2017-11-18 16:07:07 +0100

    initial commit

---
+++ src/main/java/com/clubdynamics/core/ClubdynamicsApp.java
@@ -0,0 +1,12 @@
+package com.clubdynamics.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubdynamicsApp {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(ClubdynamicsApp.class, args);
+  }
+}
